9bc9f1450f1def14613bce57cf32946a55f6972b
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2021-02-23 02:02:34 +0600

    Heroku deploying

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://emdlqijobdosfz:a2d0566f87bd79d998d9bb2b7d4b3881d234735ba679dfea697571c7faae88ca@ec2-3-222-127-167.compute-1.amazonaws.com:5432/d6c7cmvrm98cm7');
+
 return [
 
     /*
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
